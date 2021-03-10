fd290f4609c8f02e12cb1286cf4a71294e9914b3
GigaVardia <gigavardia@gmail.com>
2021-03-10 21:18:22 +0400

    finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import {addCustomerAction, removeCustomerAction} from './store/customerReducer';
 import './App.css';
 
 function App() {
+  const dispatch = useDispatch();
+  const cash = useSelector(state => state.cash.cash);
+  const customers = useSelector(state => state.customers.customers)
+  console.log(cash);
+
+  const addCash = () => {
+    dispatch({type: "ADD_CASH", payload: 1})
+  }   
+
+  const getCash = () => { 
+    dispatch({type: "GET_CASH", payload: 1})
+  }
+
+  const addCustomer = name => {
+    const customer = {
+      name: name,
+      id: Date.now()
+    }
+    dispatch(addCustomerAction(customer))
+  }
+
+  const removeCustomer = customer => {
+    console.log(removeCustomerAction(customer))
+    console.log(customers)
+    dispatch(removeCustomerAction(customer.id))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        {cash}
+      </div>
+      <div>
+        <Button onClick={addCash}>Add Cash</Button>
+        <Button onClick={getCash}>Get Cash</Button>
+        <Button onClick={() => addCustomer(prompt())}>Add client</Button>
+      </div>
+      {customers.length > 0 ? 
+        <div>
+          {customers.map(customer => 
+            <div onClick={() => removeCustomer(customer)}>{customer.name}</div>
+          )}
+        </div> 
+        : 
+        <div>
+          Clients not exists!
+        </div>
+      }
     </div>
   );
 }
---
+++ src/store/index.js
@@ -0,0 +1,10 @@
+import {createStore, combineReducers} from 'redux';
+import {cashReducer} from './cashReducer';
+import {customReducer} from './customerReducer';
+
+const rootReducer = combineReducers({
+    cash: cashReducer,
+    customers: customReducer
+})
+
+export const store = createStore(rootReducer)
